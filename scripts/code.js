520090e1d7c15f30af72e147f7b244d127626342
tyni99 <topias.tyni@hotmail.com>
2023-04-25 16:22:45 +0300

    format document

--- scripts/code.js
+++ scripts/code.js
@@ -5,29 +5,29 @@ let div = document.getElementById("divi");
 let checkbox = document.getElementById("important");
 
 const d = new Date();
- 
 
-function addNote(){
 
-    
+function addNote() {
+
+
 
     let h6 = document.createElement("h6");
     let h5 = document.createElement("h5")
     let p = document.createElement("p");
-    
-    h6.textContent= nimi.value;
-    h5.textContent= viesti.value;
+
+    h6.textContent = nimi.value;
+    h5.textContent = viesti.value;
     p.textContent = d;
 
     //onko checkboxissa täppi vai ei
 
-    if(checkbox.checked){
+    if (checkbox.checked) {
         h6.classList.add("important");
         h5.classList.add("important");
         p.classList.add("important");
     }
     //kaikki elementit divin sisälle
-    div.append(h6,h5,p, document.createElement("hr"));
-    
+    div.append(h6, h5, p, document.createElement("hr"));
+
 }
 
